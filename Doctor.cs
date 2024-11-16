205b74f2b4e2410eeb0922faf5197334425d4bf2
Mashayla Banks <121362585+mashayla@users.noreply.github.com>
2024-11-16 15:03:19 -0500

    Add files via upload

--- Doctor.cs
+++ Doctor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +9,35 @@ namespace Telehealth_Hackathon2024
 {
     public class Doctor
     {
+        public int DoctorId { get;set;}
+        public string Name {get;set;}
 
+    public Doctor(int doctorId, string name)
+    {
+        doctorId = doctorId;
+        Name=name;
+    }
+    public void WriteNote(Patient p , string noteContent)
+    {
+        if (patientpage ==null)
+            throw new ArgumentNullException(nameof(patient), "Patient can't be null,");
+            var newNote = new Note
+            { 
+                Create = noteContent,
+                TimeStamp = DateTime.Now
+            };
+            p.Notes.Add(newNote);
+    }
+
+    }
+
+    public class Note
+    {
+        public string Create { get;set;}
+        public DateTime TimeStamp {get; set;}
+    }
+    public class Patient
+    {
+        public List<Notes> Notes { get; set; } = new List<Note>();
     }
 }
